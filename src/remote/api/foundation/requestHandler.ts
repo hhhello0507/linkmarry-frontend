4d40dff2bf45afa8cad4ff48bff4a45bdc56c167
hhhello0507 <hhhello0507@gmail.com>
2025-01-12 09:52:00 +0000

    맞춤법;

--- src/remote/api/foundation/requestHandler.ts
+++ src/remote/api/foundation/requestHandler.ts
@@ -8,7 +8,7 @@ const requestHandler = (config: InternalAxiosRequestConfig) => {
     if (Cookies.get('accessToken') === undefined || Cookies.get('refreshToken') === undefined) {
         console.error('token is undefined');
         window.location.href = '/';
-        alert('로그인 해 주세요');
+        alert('로그인해 주세요');
         return config;
     }
 
