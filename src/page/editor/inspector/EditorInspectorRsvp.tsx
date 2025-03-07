2f70149429c994bdb0357bb5621c0cc23a721bbe
hhhello0507 <hhhello0507@gmail.com>
2025-03-07 00:12:47 +0000

    rsvp

--- src/page/editor/inspector/EditorInspectorRsvp.tsx
+++ src/page/editor/inspector/EditorInspectorRsvp.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import {Column, Row} from "@designsystem/core/FlexLayout";
 import Text from "@designsystem/component/Text";
-import Spacer from "@designsystem/component/Spacer";
-import Toggle from "@designsystem/component/Toggle";
 import Input from "@designsystem/component/Input";
 import Textarea from "@designsystem/component/Textarea";
 import {css} from "styled-components";
@@ -11,7 +9,6 @@ import FormToggleSet from "@designsystem/component/FormToggleSet";
 import EditorInspectorWrapper from "@page/editor/inspector/EditorInspectorWrapper";
 import Divider from "@designsystem/component/Divider";
 import Binding from "@src/interface/Binding";
-import Rsvp from "@remote/value/Rsvp";
 import WeddingDto from "@remote/value/WeddingDto";
 
 interface Props extends Binding<WeddingDto> {
@@ -25,9 +22,10 @@ const EditorInspectorRsvp = (
 ) => {
     return (
         <EditorInspectorWrapper type={'rsvp'} toggle={{
-            checked: false,
-            OnChange: () => {
-            }
+            checked: rsvp.rsvpActivate,
+            OnChange: checked => update(draft => {
+                draft.rsvp.rsvpActivate = checked;
+            })
         }}>
             <Column $alignItems={'stretch'} $gap={12}>
                 <Text type={'p3'} bold={true}>제목</Text>
@@ -49,15 +47,20 @@ const EditorInspectorRsvp = (
             <Column $alignItems={'stretch'} $gap={12}>
                 <Text type={'p3'} bold={true}>표시 항목</Text>
                 <FormToggleSet>
-                    <FormToggle checked={rsvp.attendMealStatus} OnChange={() => update(draft => {
+                    <FormToggle checked={rsvp.attendMealStatus} OnChange={checked => update(draft => {
+                        draft.rsvp.attendMealStatus = checked;
                     })} label={'식사 여부'}/>
-                    <FormToggle checked={rsvp.attendGuestCntStatus} OnChange={() => update(draft => {
+                    <FormToggle checked={rsvp.attendGuestCntStatus} OnChange={checked => update(draft => {
+                        draft.rsvp.attendGuestCntStatus = checked;
                     })} label={'참석 인원'}/>
-                    <FormToggle checked={rsvp.attendPhoneStatus} OnChange={() => update(draft => {
+                    <FormToggle checked={rsvp.attendPhoneStatus} OnChange={checked => update(draft => {
+                        draft.rsvp.attendPhoneStatus = checked;
                     })} label={'연락처'}/>
-                    <FormToggle checked={rsvp.attendBusStatus} OnChange={() => update(draft => {
+                    <FormToggle checked={rsvp.attendBusStatus} OnChange={checked => update(draft => {
+                        draft.rsvp.attendBusStatus = checked;
                     })} label={'버스 탑승 여부'}/>
-                    <FormToggle checked={rsvp.attendEtcStatus} OnChange={() => update(draft => {
+                    <FormToggle checked={rsvp.attendEtcStatus} OnChange={checked => update(draft => {
+                        draft.rsvp.attendEtcStatus = checked;
                     })} label={'추가 전달사항'}/>
                 </FormToggleSet>
             </Column>
