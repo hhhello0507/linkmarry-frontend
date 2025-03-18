a01fa09f6fee1c5526e051ac20ef23d8a45689e4
hhhello0507 <hhhello0507@gmail.com>
2025-03-18 12:37:52 +0000

    a

--- src/component/wedding/component/WaterMarkSheet.tsx
+++ src/component/wedding/component/WaterMarkSheet.tsx
@@ -1,11 +1,10 @@
-import React, {useEffect, useRef} from 'react';
-import {Column, Row} from "@designsystem/core/FlexLayout";
+import React, {useState} from 'react';
+import {Column} from "@designsystem/core/FlexLayout";
 import styled, {css} from "styled-components";
 import fadeInAnimationStyle from "@designsystem/animation/fadeInAnimationStyle";
 import Button from "@designsystem/component/Button";
-import {useNavigate} from "react-router-dom";
 import Text from "@designsystem/component/Text";
-import {NAVER_STORE_URL, NAVER_STORE_WEDDING_URL} from "@util/constant";
+import RemoveWatermarkDialog from "@src/component/dialog/RemoveWatermarkDialog";
 
 interface Props {
     url: string;
@@ -16,11 +15,17 @@ function WaterMarkSheet(
         url,
     }: Props
 ) {
-    const navigate = useNavigate();
+    const [showRemoveWatermarkDialog, setShowRemoveWatermarkDialog] = useState(false);
 
     return (
         <SheetStyle>
-            <Column $gap={28} $alignItems={'center'} $ui={css`
+            {showRemoveWatermarkDialog && (
+                <RemoveWatermarkDialog
+                    url={url}
+                    dismiss={() => setShowRemoveWatermarkDialog(false)}
+                />
+            )}
+            <Column $gap={28} $alignItems={'stretch'} $ui={css`
                 padding: 32px 24px;
                 background: white;
                 border-radius: 16px;
@@ -39,21 +44,12 @@ function WaterMarkSheet(
                 <Text type={'p3'} ui={css`
                     color: var(--g-600);
                     text-align: center;
-                `}>결제 후 대시보드 &gt; 워터마크 제거 <br/>
+                `}>결제 후 마이페이지 &gt; 에디터 &gt; 워터마크 제거,
+                    또는 에디터 상단의 워터마크 제거
                     버튼을 통해 제거할 수 있습니다.</Text>
-                <Row $gap={12} $alignSelf={'stretch'}>
-                    <Button text={'구매하기'} buttonType={'outlined'} ui={css`
-                        flex: 1;
-                    `} onClick={() => {
-                        window.open(NAVER_STORE_URL);
-                    }}/>
-                    <Button text={'워터마크 제거하기'} style={{
-                        flex: 1
-                    }} onClick={() => {
-
-                        // navigate(`/dashboard/design/${url}`);
-                    }}/>
-                </Row>
+                <Button text={'워터마크 제거하기'} buttonType={'tonal'} onClick={() => {
+                    setShowRemoveWatermarkDialog(true);
+                }}/>
             </Column>
         </SheetStyle>
     );
@@ -62,10 +58,6 @@ function WaterMarkSheet(
 const SheetStyle = styled.div`
     display: flex;
     position: fixed;
-    //width: 100vw;
-    //min-width: 100vw;
-    //height: 100vh;
-    //min-height: 100vh;
     bottom: 0;
     left: 50%;
     justify-content: center;
@@ -73,6 +65,7 @@ const SheetStyle = styled.div`
     border: none;
     outline: none;
     background: none;
+    z-index: 100;
     ${fadeInAnimationStyle};
 `
 
