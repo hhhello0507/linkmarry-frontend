03ba7d29f293f9e8f9e9da433e64180dc3555416
hhhello0507 <hhhello0507@gmail.com>
2025-03-18 12:43:26 +0000

    a

--- src/component/wedding/dialog/guestcomment/CreateGuestCommentDialog.tsx
+++ src/component/wedding/dialog/guestcomment/CreateGuestCommentDialog.tsx
@@ -1,11 +1,12 @@
 import React, {useRef} from 'react';
 import BaseDialog, {applyBaseDialogContent} from "@designsystem/pattern/dialog/BaseDialog";
-import styled, {css} from "styled-components";
+import {css} from "styled-components";
 import {Column} from "@designsystem/core/FlexLayout";
 import Text from "@designsystem/component/Text";
 import Button from "@designsystem/component/Button";
 import weddingApi from "@remote/api/WeddingApi";
-import {useNavigate} from "react-router-dom";
+import Input from "@designsystem/component/Input";
+import Textarea from "@designsystem/component/Textarea";
 
 interface CreateGuestCommentDialogProps {
     url: string;
@@ -23,8 +24,6 @@ function CreateGuestCommentDialog(
     const nameRef = useRef<HTMLInputElement>(null);
     const passwordRef = useRef<HTMLInputElement>(null);
     const commentRef = useRef<HTMLTextAreaElement>(null);
-
-    const navigate = useNavigate();
 
     const onClickSave = async () => {
         if (!nameRef.current?.value) {
@@ -59,36 +58,29 @@ function CreateGuestCommentDialog(
 
     return (
         <BaseDialog dismiss={dismiss}>
-            <S.container>
+            <Column $gap={48} $alignItems={'stretch'} $ui={css`
+                width: 80vw;
+                max-width: 436px;
+                padding: 44px 36px;
+                background: white;
+                border-radius: 12px;
+                ${applyBaseDialogContent()};
+            `}>
                 <Column $gap={4} $alignItems={'center'}>
-                    {/*<Text type={'h6'}>방명록 작성하기</Text>*/}
+                    <Text type={'p1'} bold={true}>방명록 작성하기</Text>
                     <Text type={'caption1'} ui={css`
                         color: var(--g-400);
                     `}>저희에게 따뜻한 말을 남겨주세요</Text>
                 </Column>
                 <Column $gap={12} $alignItems={'stretch'}>
-                    {/*<TextField ref={nameRef} placeholder={'성함'}/>*/}
-                    {/*<TextField ref={passwordRef} placeholder={'비밀번호 입력'} fieldProps={{type: 'password'}}/>*/}
-                    {/*<Textarea ref={commentRef} placeholder={'200자 이내로 작성'} maxLength={200}/>*/}
+                    <Input ref={nameRef} placeholder={'성함'}/>
+                    <Input ref={passwordRef} placeholder={'비밀번호 입력'} type={'password'}/>
+                    <Textarea ref={commentRef} placeholder={'200자 이내로 작성'} maxLength={200}/>
                 </Column>
                 <Button text={'방명록 남기기'} onClick={onClickSave}/>
-            </S.container>
+            </Column>
         </BaseDialog>
     );
-}
-
-const S = {
-    container: styled.div`
-        display: flex;
-        width: 80vw;
-        max-width: 560px;
-        flex-direction: column;
-        padding: 44px 36px;
-        gap: 48px;
-        background: white;
-        border-radius: 12px;
-        ${applyBaseDialogContent()};
-    `
 }
 
 export default CreateGuestCommentDialog;
