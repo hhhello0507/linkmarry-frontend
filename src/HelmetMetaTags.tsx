30e55b04991b653b75515444b9d9a2c060f917b0
hhhello0507 <hhhello0507@gmail.com>
2025-03-20 00:22:29 +0000

    a

--- src/HelmetMetaTags.tsx
+++ src/HelmetMetaTags.tsx
@@ -7,12 +7,14 @@ const HelmetMetaTags = () => {
             {/*검색 엔진 최적화(SEO) 관련 메타 태그*/}
             <title>링크메리 - 특별한 순간 특별한 초대, 링크메리와 함께</title>
             <meta name="description" content="링크메리와 함께 결혼을 더욱 특별하게. 이제, 종이 청첩장 대신 스마트폰으로 간편하고 빠르게 초대장을 보내보세요"/>
-            <meta name="keywords" content="링크메리, 스마트 청첩장, 모바일 초대장, 온라인 청첩장, 결혼식 초대장, 디지털 청첩장, 웨딩 초대장"/>
+            <meta name="keywords"
+                  content="모바일청첩장,모바일 초대장,셀프청첩장,무료모바일청첩장,결혼식 초대장,청첩장,결혼준비"/>
             <meta name="author" content="링크메리"/>
 
             {/*Open Graph (SNS 공유 최적화)*/}
             <meta property="og:title" content="링크메리"/>
-            <meta property="og:description" content="특별한 순간 특별한 초대, 링크메리와 함께. 이제, 종이 청첩장 대신 스마트폰으로 간편하고 빠르게 초대장을 보내보세요"/>
+            <meta property="og:description"
+                  content="특별한 순간 특별한 초대, 링크메리와 함께. 이제, 종이 청첩장 대신 스마트폰으로 간편하고 빠르게 초대장을 보내보세요"/>
             <meta property="og:image" content="%PUBLIC_URL%/ogimage.png"/>
             <meta property="og:url" content="https://www.linkmarry.com/"/>
             <meta property="og:type" content="website"/>
