0a2862d5e6babc216f2bb95fa35eb4adf73ad4ce
hhhello0507 <hhhello0507@gmail.com>
2025-05-21 10:44:21 +0000

    카카오 공유 기능 구현

--- src/userinterface/specific/wedding/component/FooterTemplate.tsx
+++ src/userinterface/specific/wedding/component/FooterTemplate.tsx
@@ -1,20 +1,91 @@
-import React, {ComponentPropsWithoutRef} from 'react';
+import React, {ComponentPropsWithoutRef, useCallback} from 'react';
 import {Column, Row} from "@src/userinterface/core/FlexLayout";
 import Text from "@src/userinterface/component/Text";
 import {css} from "styled-components";
 import {backgroundStyle} from "@src/infrastructure/network/value/WeddingDesign";
 import Icon, {IconType} from "@src/userinterface/foundation/Icon";
+import LinkShare from "@src/infrastructure/network/value/LinkShare";
+import {getWeddingUrl} from "@src/shared/string-util";
+import KakaoButton from "@src/infrastructure/network/enumeration/KakaoButton";
+import WeddingPlace from "@src/infrastructure/network/value/WeddingPlace";
 
 interface FooterTemplateProps extends ComponentPropsWithoutRef<'div'> {
+    url: string;
     background: string;
+    linkShare: LinkShare;
+    weddingPlace: WeddingPlace;
 }
 
 function FooterTemplate(
     {
+        url,
         background,
+        linkShare,
+        weddingPlace,
         ...props
     }: FooterTemplateProps
 ) {
+    const shareToKakao = useCallback(() => {
+        const {Kakao: {Link}} = window as any;
+        const weddingUrl = getWeddingUrl(url);
+
+        const buttonRecord: Record<KakaoButton, {
+            title: string;
+            link: {
+                mobileWebUrl: string;
+                webUrl: string;
+            };
+        }[]> = {
+            [KakaoButton.NONE]: [],
+            [KakaoButton.PLACE]: [
+                {
+                    title: '모바일청첩장',
+                    link: {
+                        mobileWebUrl: weddingUrl,
+                        webUrl: weddingUrl
+                    }
+                },
+                {
+                    title: '위치보기',
+                    link: {
+                        mobileWebUrl: weddingPlace.placeUrl,
+                        webUrl: weddingPlace.placeUrl
+                    }
+                }
+            ],
+            [KakaoButton.ATTEND]: [
+                {
+                    title: '모바일청첩장',
+                    link: {
+                        mobileWebUrl: weddingUrl,
+                        webUrl: weddingUrl
+                    }
+                },
+                {
+                    title: '참석의사 전달',
+                    link: {
+                        mobileWebUrl: weddingUrl, // todo: add query
+                        webUrl: weddingUrl
+                    }
+                }
+            ]
+        }
+        Link.sendDefault({
+            objectType: 'feed',
+            content: {
+                title: linkShare.kakaoTitle,
+                description: linkShare.kakaoContent,
+                imageUrl: linkShare.kakaoImgUrl,
+                link: {
+                    mobileWebUrl: url,
+                    webUrl: url
+                }
+            },
+            buttons: buttonRecord[linkShare.kakaoButton],
+            installTalk: true,
+        });
+    }, [linkShare.kakaoButton, linkShare.kakaoContent, linkShare.kakaoImgUrl, linkShare.kakaoTitle, url, weddingPlace.placeUrl]);
+
     return (
         <Column $alignItems={'stretch'} $ui={css`
             background: ${backgroundStyle(background)};
@@ -27,7 +98,9 @@ function FooterTemplate(
                 }}
                 $alignItems={'center'}
             >
-                <Row $gap={8} $alignItems={'center'}>
+                <Row $gap={8} $alignItems={'center'} $ui={css`
+                    cursor: pointer;
+                `} onClick={shareToKakao}>
                     <Icon iconType={IconType.Kakao} size={20}/>
                     <Text size={14} weight={300} ui={css`
                         color: var(--g-600);
