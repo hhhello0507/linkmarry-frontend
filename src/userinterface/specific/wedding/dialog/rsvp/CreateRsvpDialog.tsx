1261e9c8b5215a90c1e0d263940257ccef54a645
hhhello0507 <hhhello0507@gmail.com>
2025-08-02 16:47:42 +0000

    RSVP guestType 수정

--- src/userinterface/specific/wedding/dialog/rsvp/CreateRsvpDialog.tsx
+++ src/userinterface/specific/wedding/dialog/rsvp/CreateRsvpDialog.tsx
@@ -42,7 +42,7 @@ function CreateRsvpDialog(
     const createRsvp = async () => {
         await weddingApi.createRsvp({
             url,
-            guestType: guestType === 0 ? GuestType.GROOM : GuestType.BRIDE,
+            guestType: guestType === 0 ? GuestType.BRIDE : GuestType.GROOM,
             isAttend: isAttend === 0,
             isMeal: isMeal === 0,
             guestName: guestName,
